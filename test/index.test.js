c05b4d4e9db1d789b222413535fbb1f87eb8f72a
geekplux <geekplux@qq.com>
2017-04-04 12:03:10 +0800

    update dev; test render

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,18 @@
-import markdownItFence from '../src'
+import markdownIt from 'markdown-it'
+import mdFence from '../src'
+const md = markdownIt()
 
 test('main', () => {
-  expect(typeof markdownItFence).toBe('function')
+  expect(typeof mdFence).toBe('function')
+})
+
+test('render', () => {
+  const testStr = `
+    \`\`\`test
+    I'm testing
+    \`\`\`
+  `
+  const plugin = () => mdFence(md, 'test')
+  console.log(plugin)
+  expect(md.use(plugin).render(testStr)).toBe(`<pre><code>\`\`\`test\nI'm testing\n\`\`\`\n</code></pre>\n`)
 })
